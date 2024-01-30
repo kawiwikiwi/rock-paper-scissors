c8941b6f15e8cde382585fc9938938a8f339958b
kawiwikiwi <kawiwikiwi@outlook.com>
2024-01-30 14:50:14 +0000

    add playRound function

--- script.js
+++ script.js
@@ -12,11 +12,65 @@
 // return string with winner/tie 
 
 const choice = ['rock', 'paper', 'scissors'];
+let playerSelection;
 
 function getComputerChoice() {
     const randomSelection = Math.floor(Math.random() * choice.length);
     const computerSelection = choice[randomSelection];
+
     return computerSelection;
 }
 
 console.log(getComputerChoice());
+
+function playRound() {
+   let userInput = prompt('Rock, Paper, Scissors', '');
+   playerSelection = userInput.toLowerCase();
+   const computerSelection = getComputerChoice();
+
+   console.log('Player Selection:', playerSelection);
+   console.log('Comp Selection:', computerSelection);
+
+   let result;
+   const playerWin = 'You Won!';
+   const playerLose = 'You Lost...';
+   const tied = 'Looks Like You Tied!';
+   
+   switch (playerSelection) {
+    case 'rock':
+        if (computerSelection === 'scissors') {
+            result = playerWin
+        } else if (computerSelection === 'paper') {
+            result = playerLose
+        } else {
+            result = tied
+        }
+        break;
+    case 'paper':
+        if (computerSelection === 'rock') {
+            result = playerWin
+        } else if (computerSelection === 'scissors') {
+            result = playerLose
+        } else {
+            result = tied
+        }
+        break;
+    case 'scissors':
+        if (computerSelection === 'paper') {
+            result = playerWin
+        } else if (computerSelection === 'rock') {
+            result = playerLose
+        } else {
+            result = tied
+        }
+        break;
+    default:
+        result = 'Please enter either rock, paper or scissors to play!'
+   }
+
+   return result;
+}
+
+console.log(playRound());
+
+

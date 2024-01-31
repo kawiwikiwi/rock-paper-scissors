5bcdc5cff9d94d096f9ecfc9e6a2c6b2f4a78785
kawiwikiwi <kawiwikiwi@outlook.com>
2024-01-31 12:39:10 +0000

    Refactored code to allow for score function

--- script.js
+++ script.js
@@ -11,9 +11,6 @@
 // paper > rock, rock > scissors, scissors > paper, same === draw
 // return string with winner/tie 
 
-const choice = ['rock', 'paper', 'scissors'];
-let playerSelection;
-
 function getComputerChoice() {
     const randomSelection = Math.floor(Math.random() * choice.length);
     const computerSelection = choice[randomSelection];
@@ -21,56 +18,74 @@ function getComputerChoice() {
     return computerSelection;
 }
 
-console.log(getComputerChoice());
+
+
+function getPlayerChoice() {
+    let userInput = prompt('Rock, Paper, Scissors', '');
+    playerSelection = userInput.toLowerCase();
+
+    return playerSelection;
+}
+
+
 
 function playRound() {
-   let userInput = prompt('Rock, Paper, Scissors', '');
-   playerSelection = userInput.toLowerCase();
+   const playerSelection = getPlayerChoice();
    const computerSelection = getComputerChoice();
 
    console.log('Player Selection:', playerSelection);
    console.log('Comp Selection:', computerSelection);
-
-   let result;
-   const playerWin = 'You Won!';
-   const playerLose = 'You Lost...';
-   const tied = 'Looks Like You Tied!';
    
-   switch (playerSelection) {
-    case 'rock':
-        if (computerSelection === 'scissors') {
-            result = playerWin
-        } else if (computerSelection === 'paper') {
-            result = playerLose
-        } else {
-            result = tied
-        }
-        break;
-    case 'paper':
-        if (computerSelection === 'rock') {
-            result = playerWin
-        } else if (computerSelection === 'scissors') {
-            result = playerLose
-        } else {
-            result = tied
-        }
-        break;
-    case 'scissors':
-        if (computerSelection === 'paper') {
-            result = playerWin
-        } else if (computerSelection === 'rock') {
-            result = playerLose
-        } else {
-            result = tied
-        }
-        break;
-    default:
-        result = 'Please enter either rock, paper or scissors to play!'
-   }
+   const result = roundResult(playerSelection, computerSelection);
 
    return result;
+
 }
 
+function roundResult(playerSelection, computerSelection) {
+
+    let result;
+    const playerWin = 'You Won!';
+    const playerLose = 'You Lost...';
+    const tied = 'Looks Like You Tied!';
+
+
+    switch (playerSelection) {
+        case 'rock':
+            if (computerSelection === 'scissors') {
+                result = playerWin
+            } else if (computerSelection === 'paper') {
+                result = playerLose
+            } else {
+                result = tied
+            }
+            break;
+        case 'paper':
+            if (computerSelection === 'rock') {
+                result = playerWin
+            } else if (computerSelection === 'scissors') {
+                result = playerLose
+            } else {
+                result = tied
+            }
+            break;
+        case 'scissors':
+            if (computerSelection === 'paper') {
+                result = playerWin
+            } else if (computerSelection === 'rock') {
+                result = playerLose
+            } else {
+                result = tied
+            }
+            break;
+        default:
+            result = 'Please enter either rock, paper or scissors to play!'
+       }
+    return result;
+}
+
+const choice = ['rock', 'paper', 'scissors'];
+let playerSelection;
 console.log(playRound());
 
 
